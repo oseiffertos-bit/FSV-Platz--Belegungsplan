0a50a53b6e14d6f25849b41e2c332b2409fed91c
oseiffertos-bit <o.seiffert.os@gmail.com>
2026-06-16 22:42:48 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,6 +5,6 @@ import { createClient } from "@supabase/supabase-js";
 // (Findest du in Supabase unter: Project Settings → API)
 // ════════════════════════════════════════════════════════════
 const SUPABASE_URL = "https://imljstabxenkwatbubsw.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltbGpzdGFieGVua3dhdGJ1YnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDk5NzYsImV4cCI6MjA5NzEyNTk3Nn0.7tZx2C9gWNEhxRDGUldTgXMJjpUiFceyrR6sNuZ44mU;"DEIN_SUPABASE_ANON_KEY_HIER";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltbGpzdGFieGVua3dhdGJ1YnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDk5NzYsImV4cCI6MjA5NzEyNTk3Nn0.7tZx2C9gWNEhxRDGUldTgXMJjpUiFceyrR6sNuZ44mU;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
