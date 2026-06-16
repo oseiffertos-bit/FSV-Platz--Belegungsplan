7f50dc6858ee06f24f1f3a3586ec3c5bd852eb22
oseiffertos-bit <o.seiffert.os@gmail.com>
2026-06-16 22:58:57 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 // Hier deine Supabase-Zugangsdaten eintragen
 // (Findest du in Supabase unter: Project Settings → API)
 // ════════════════════════════════════════════════════════════
-const SUPABASE_URL = "https://imljstabxenkwatbubsw.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltbGpzdGFieGVua3dhdGJ1YnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDk5NzYsImV4cCI6MjA5NzEyNTk3Nn0.7tZx2C9gWNEhxRDGUldTgXMJjpUiFceyrR6sNuZ44mU;
+const SUPABASE_URL = "https://imljstabxenkwatbubsw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltbGpzdGFieGVua3dhdGJ1YnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDk5NzYsImV4cCI6MjA5NzEyNTk3Nn0.7tZx2C9gWNEhxRDGUldTgXMJjpUiFceyrR6sNuZ44mU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
